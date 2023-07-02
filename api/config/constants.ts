95083d2199d22fd4d22f928772b68e8a7dab6208
kumarsathvik <63970676+kumarsathvik@users.noreply.github.com>
2023-07-02 13:08:40 -0700

    Update constants.ts

--- api/config/constants.ts
+++ api/config/constants.ts
@@ -3,6 +3,6 @@ export const constants = {
   PORT: process.env.PORT || 4000,
   URI: process.env.URI || 'http://localhost:3000',
   MONGODB_URI:
-    process.env.MONGODB_URI || 'mongodb+srv://sathvik:mongodb2023@cluster0.7i3v0zu.mongodb.net/?retryWrites=true',
+    process.env.MONGODB_URI || 'mongodb+srv://sathvikkum:402S5cRiqOqP9tkI@cluster0.awsa7nu.mongodb.net/?retryWrites=true',
   JWT_SECRET: 'dog**&&unning***GrassyField$$$Sunny%%%ay!!!',
 };
